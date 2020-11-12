776bd49ef7aef2c3fe6acc57de95fd3cc1ee96ba
Yan <yankaiquecosta.yk@gmail.com>
2020-11-11 22:30:38 -0300

    create service incomplete

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,44 @@
+import { getCustomRepository, getRepository } from 'typeorm';
 // import AppError from '../errors/AppError';
-
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Category from '../models/Category';
+
+interface Request {
+  id: string;
+  title: string;
+  type: 'income' | 'outcome';
+  category: Category;
+  value: number;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category
+  }: Request): Promise<Transaction> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+
+    const findCategory = await transactionRepository.findOne({
+      where: { category },
+    });
+    const categoryRepository = getRepository(Category);
+
+    if (!findCategory) {
+      categoryRepository.
+      const newCategory = categoryRepository.create({
+        title: category,
+      });
+
+      await categoryRepository.save(newCategory);
+    }else{
+      const presentCategory = categoryRepository.getId({ where: { category }})
+    }
+
+
+    transactionRepository.create({})
   }
 }
 
